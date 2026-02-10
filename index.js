b24e8fd9f27e4ea092f55286087bc2b78926e894
sokorecords <sokorecords@gmail.com>
2026-02-10 08:15:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -786,6 +786,7 @@ app.get('/api/admin/all-users', verifyAdmin, async (req, res) => {
         username,
         fragmentsCount: realFragments.length,
         collection: realFragments,
+        streak: realstreak,
         streakStored: streak?.streak || 0,
         streakReal: realStreak,
         streakStatus: streakStatus,
@@ -1215,6 +1216,7 @@ app.post('/api/admin/update-pioneer-url', verifyAdmin, async (req, res) => {
 });
 
 export default app;
+
 
 
 
